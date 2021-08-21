01b87c8cd587c7e62f978f54b1b45e3807e7811f
Nicolas <nicolas.mazaud.sio@gmail.com>
2021-08-21 17:18:35 +0200

    Création de la bdd et du userModel

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,27 @@
 import express from "express";
+import mongoose from 'mongoose';
 import data from './data.js';
+import userRouter from "./routers/userRouter.js";
 
 const app = express();
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/espaceinsolite', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
 
 app.get('/api/products', (req, res) => {
     res.send(data.products);
 });
 
+app.use('/api/users', userRouter);
+
 app.get('/', (req, res) => {
     res.send('server is ready');
+});
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;

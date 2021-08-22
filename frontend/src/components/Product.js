afc7066f0fbb09278946305ec2ad3089dcd4e892
Martins Vieira Samuel <samuelmvpro@gmail.com>
2021-08-22 16:54:14 +0200

    ecran panier + supression

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,22 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Rating from './Rating';
 
 export default function Product(props){
-    const { product } = props;
-    return (
-        <div key={product._id} className="card">
-                        <a href={`/product/${product._id}`}>
-                            <img src={product.image} alt="bulle" />
-                        </a>
-                        <div className="card-body">
-                          <a href={`/product/${product._id}`}>
-                                <h2>{product.name}</h2>
-                            </a>
-                            <Rating rating={product.rating} numReviews={product.numReviews}></Rating>
-                            <div className="price">
-                              {product.price}€
-                            </div>
-                        </div>
-                      </div>
-    )
+  const { product } = props;
+  return (
+    <div key={product._id} className="card">
+      <Link to={`/product/${product._id}`}>
+        <img src={product.image} alt="bulle" />
+      </Link>
+      <div className="card-body">
+        <Link to={`/product/${product._id}`}>
+          <h2>{product.name}</h2>
+        </Link>
+        <Rating rating={product.rating} numReviews={product.numReviews}></Rating>
+        <div className="price">
+          {product.price}€
+        </div>
+      </div>
+    </div>
+  );
 }
